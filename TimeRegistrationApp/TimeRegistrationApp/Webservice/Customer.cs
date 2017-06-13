65a09e57f9197fee7ea4eb03392c8c593560fe1d
Determinatoro <sporvogn@live.com>
2017-06-13 20:23:54 +0200

    upload

---
+++ TimeRegistrationApp/TimeRegistrationApp/Webservice/Customer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeRegistrationApp.Webservice
+{
+    public class Customer
+    {
+        private int customerId;
+        private string name;
+
+        public Customer(int customerId, string name)
+        {
+            this.customerId = customerId;
+            this.name = name;
+        }
+
+        public Customer()
+        {
+
+        }
+
+        public int CustomerId
+        {
+            get
+            {
+                return customerId;
+            }
+
+            set
+            {
+                customerId = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+    }
+}
