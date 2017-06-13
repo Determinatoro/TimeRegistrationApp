612fb9edafd997e44ff2b0f07a3ffabd2a4af803
Determinatoro <sporvogn@live.com>
2017-06-13 20:58:20 +0200

    Upload

--- TimeRegistrationApp/TimeRegistrationApp/Webservice/TimeRegistration.cs
+++ TimeRegistrationApp/TimeRegistrationApp/Webservice/TimeRegistration.cs
@@ -8,10 +8,122 @@ namespace TimeRegistrationApp.Webservice
 {
     class TimeRegistration
     {
-        private DateTime startTime;
-        private DateTime endTime;
+        private int timeRegId;
+        private string startTime;
+        private string endTime;
+        private string total;
         private int orderId;
         private int userId;
+        private string description;
         private string note;
+
+        public TimeRegistration()
+        {
+
+        }
+
+        public int TimeRegId
+        {
+            get
+            {
+                return timeRegId;
+            }
+
+            set
+            {
+                timeRegId = value;
+            }
+        }
+
+        public string StartTime
+        {
+            get
+            {
+                return startTime;
+            }
+
+            set
+            {
+                startTime = value;
+            }
+        }
+
+        public string EndTime
+        {
+            get
+            {
+                return endTime;
+            }
+
+            set
+            {
+                endTime = value;
+            }
+        }
+
+        public string Total
+        {
+            get
+            {
+                return total;
+            }
+
+            set
+            {
+                total = value;
+            }
+        }
+
+        public int OrderId
+        {
+            get
+            {
+                return orderId;
+            }
+
+            set
+            {
+                orderId = value;
+            }
+        }
+
+        public int UserId
+        {
+            get
+            {
+                return userId;
+            }
+
+            set
+            {
+                userId = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+
+            set
+            {
+                description = value;
+            }
+        }
+
+        public string Note
+        {
+            get
+            {
+                return note;
+            }
+
+            set
+            {
+                note = value;
+            }
+        }
     }
 }
